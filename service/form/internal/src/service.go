e7a0f16c904e5809479dbfdd9cfafb1de2ccfeae
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-04-20 12:30:59 +0800

    feat: 🎸 added form service

---
+++ service/form/internal/src/service.go
@@ -0,0 +1,20 @@
+package src
+
+import (
+	"github.com/mrspec7er/license-request/service/form/internal/db"
+	"gorm.io/gorm"
+)
+
+type FormService struct {
+	DB *gorm.DB
+}
+
+func (s FormService) GetOne(form *db.Form) (int, error) {
+	err := s.DB.Preload("Sections").Preload("Sections.Fields").First(&form).Error
+
+	if err != nil {
+		return 500, err
+	}
+
+	return 200, nil
+}
